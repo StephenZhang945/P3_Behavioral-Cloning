6798835bb796505dde8ab732eafe34886069239b
Stephen <34086771+StephenZhang945@users.noreply.github.com>
2018-07-14 21:47:21 +0800

    Update clone.py

--- clone.py
+++ clone.py
@@ -26,7 +26,8 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(input_shape = (160, 320, 3)))
+model.add(Lambda(lambda x : x / 255.0, input_shape(160, 320, 3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
